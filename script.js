474aa97b1aea844cafe02cb851a6975ef64b9ad0
João Vitor Pontes Leão <99336005+joaoleaodev@users.noreply.github.com>
2026-03-17 21:33:48 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,141 @@
+// console.log("João")
+
+// console.info("Informação")
+
+// console.warn("Aviso")
+
+// console.error("Erro")
+
+// ---------------------------------------------------
+
+// console.table([
+//     {
+//         id: 1, tarefa: "Estudar JS"
+//     },
+//     {
+//         id: 2, tarefa: "Praticar código"
+//     }
+// ])
+
+// -----------------------------------------------------
+
+// console.group("Grupo de logs")
+// console.log("log 1")
+// console.log("log 2")
+// console.log("log 3")
+// console.groupEnd()
+
+// -----------------------------------------------------
+
+// console.time("Timer")
+
+// let casa = ""
+// if (casa = "casa") {
+//     console.log("casa")
+// }
+
+// console.timeEnd("Timer")
+
+// -----------------------------------------------------
+
+/* 
+sdfsdgeasadasdfes
+sdfsdgeasadasdfes
+sdfsdgeasadasdfes
+sdfsdgeasadasdfes
+sdfsdgeasadasdfes
+sdfsdgeasadasdfes 
+*/
+
+// -----------------------------------------------------
+
+// var antigo = "Forma antiga de declarar variáveis"
+
+// let userName = "João"
+
+// const API_URL = "url"
+
+// -----------------------------------------------------
+
+/* let texto = "Olá"
+console.log("nome= " + texto + " tipo: " + typeof (texto))
+
+let numero = 67
+console.log("numero= " + numero + " tipo: " + typeof (numero))
+
+let isCompleted =  false
+console.log("inCompleted= " + isCompleted + " tipo: " + typeof (isCompleted))
+
+let semValor
+console.log("semValor= " + semValor + " tipo: " + typeof (semValor))
+
+let nulo = null
+console.log("nulo= " + nulo + " tipo: " + typeof (nulo))
+
+let uniqueId = Symbol("id")
+console.log(typeof (uniqueId))
+console.log(uniqueId)
+
+let bigNumero = 999999999999999n
+console.log("bigNumero= " + bigNumero + " tipo: " + typeof (bigNumero)) */
+
+// -----------------------------------------------------
+
+/* let task = {
+  id: 1,
+  title: "Aprender JavaScript",
+  completed: false,
+  assignee: {
+    name: "João",
+    email: "joao@example.com"
+  },
+  toggleComplete() {
+    this.completed = !this.completed;
+  }
+}; */
+
+/* let tasks = [
+  { id: 1, title: "Tarefa 1", completed: false },
+  { id: 2, title: "Tarefa 2", completed: true }
+]; */
+
+/* let hoje = new Date();
+let dataFutura = new Date("03-26-2025"); */
+
+// let pattern = /^\+55\(\d{2}\)\d{4,5}\-\d{4}$/;
+// console.log(pattern.test("+55(11)92299-0899"));
+// console.log(pattern.test("+55(11)2299-0899"));
+
+// console.log(Number("67"))
+// console.log("67")
+
+// console.log(parseFloat("67.1"))
+// console.log("67")
+
+// console.log(Boolean("texto"))
+// console.log(Boolean(""))
+
+// let a = 10
+// let b = 3
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+// console.log(a ** b);
+
+let contador1 = 0
+let contador2 = 0
+console.log(++contador1)
+console.log(++contador1)
+console.log(contador2++)
+console.log(contador2++)
+
+let x = 10;
+x += 5; // x = 15
+x -= 3; // x = 12
+x *= 2; // x = 24
+x /= 4; // x = 6
+x %= 2; // x = 0
+x **= 3; // x = 0 (0 elevado a 3)
