ddb42ec74db3e576579fcbfe6891fd210ea65006
João Vitor Pontes Leão <99336005+joaoleaodev@users.noreply.github.com>
2026-03-24 20:56:15 -0300

    Create script.js

---
+++ aula2/script.js
@@ -0,0 +1,69 @@
+// let idade = 18
+// let status = idade >= 18 ?
+// "Maior de idade" : "Menor de idade"
+
+// console.log(idade)
+// console.log(status)
+
+// let valor = false
+// let resultado = valor ?? "Valor padrão"
+
+// console.log(resultado)
+
+// let tarefaConcluida = true
+// if(tarefaConcluida) {console.log("A tarefa está concluída")}
+
+// let prioridade = 2
+// if(prioridade === 3) {console.log("Prioridade alta")}
+// else {console.log("Prioridade não é alta")}
+
+// let prioridade2 = prioridade === 3 ? "Prioridade alta" : "Prioridade não é alta"
+// console.log(prioridade === 3 ? "Prioridade alta" : "Prioridade não é alta")
+
+// if(prioridade === 1) {console.log("Prioridade baixa")}
+// else if (prioridade === 2) {console.log("Prioridade média")}
+// else if (prioridade === 3) {console.log("Prioridade alta")}
+// else {console.log("Prioridade desconhecida")}
+
+// let diaSemana =  new Date().getDay()
+
+// console.log(diaSemana)
+
+// switch (diaSemana) {
+//     case 0:
+//         console.log("Domingo")
+//         break;
+//     case 1:
+//         console.log("Segunda")
+//         break;
+//     case 2:
+//         console.log("Terça")
+//         break;
+//     case 0:
+//         console.log("Quarta")
+//         break;
+//     case 0:
+//         console.log("Quinta")
+//         break;
+//     case 0:
+//         console.log("Sexta")
+//         break;
+//     case 0:
+//         console.log("Sábado")
+//         break;
+
+//     default:
+//         console.log("Dia inválido")
+//         break;
+// }
+
+// for (let i = 0;i < 5;i++) {
+//     console.log("Contagem: ", + contador)
+//     contador--
+// }
+
+// let numero = 5
+// do {
+//     console.log("Número é: ", numero)
+//     numero--
+// } while (numero > 0)
