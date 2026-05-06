5a8691250d99b996b856063711c5b4e97c7467d8
Laboratório FIAP <labsfiap@fiap.com.br>
2026-05-05 22:16:56 -0300

    aula

---
+++ aula6/script.js
@@ -0,0 +1,11 @@
+const titulo = document.quertSelector('h1')
+
+console.log("Título representante", titulo)
+
+titulo.textContent = "Novo Título"
+
+// const container = documento.getElementById("container")
+const container = documento.querySelector(".container")
+console.log(container)
+
+container 
